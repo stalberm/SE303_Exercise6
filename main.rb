d5c510e50b048deae5bfb3ce63706dcb950ef340
Micah Stalberg <stalberm@oregonstate.edu>
2022-05-08 18:09:55 -0700

    Decompose second leg of draw_button conditional into it's own function

--- main.rb
+++ main.rb
@@ -12,6 +12,10 @@ def draw_dark_mode_button(label_text, button_location, foreground_color)
     paint(label_text, button_location.x, button_location.y, foreground_color - 10, '#111111')
 end
 
+def draw_normal_button(label_text, button_location, foreground_color)
+    paint(label_text, button_location.x, button_location.y, foreground_color + 10, '#E0E0E0')
+end
+
 class ButtonLocation
     attr_reader :x, :y
     def initialize(x,y)
