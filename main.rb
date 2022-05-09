84607dae70388be8e9d0f0227df7504291a007a7
Micah Stalberg <stalberm@oregonstate.edu>
2022-05-08 17:57:59 -0700

    Remove x parameter from draw_button

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-def draw_button(label_text, x, y, button_location, foreground_color, is_dark_mode)
+def draw_button(label_text, y, button_location, foreground_color, is_dark_mode)
     if is_dark_mode
       # darken foreground color for dark mode
       paint(label_text, button_location.x, y, foreground_color - 10, '#111111')
