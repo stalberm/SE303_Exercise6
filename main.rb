ed8ad89056b2f358e010a953326ec5c42bf37f72
Micah Stalberg <stalberm@oregonstate.edu>
2022-05-08 17:57:39 -0700

    Replace x paramater useage with button location

--- main.rb
+++ main.rb
@@ -1,10 +1,10 @@
 def draw_button(label_text, x, y, button_location, foreground_color, is_dark_mode)
     if is_dark_mode
       # darken foreground color for dark mode
-      paint(label_text, x, y, foreground_color - 10, '#111111')
+      paint(label_text, button_location.x, y, foreground_color - 10, '#111111')
     else
       # lighten foreground color for non-dark mode
-      paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+      paint(label_text, button_location.x, y, foreground_color + 10, '#E0E0E0')
     end
 end
 
