fdc8b9ee8494146ae40f86bd083f28a0c5ec90fa
Micah Stalberg <stalberm@oregonstate.edu>
2022-05-08 18:18:55 -0700

    Move functions using common record into the new class

--- main.rb
+++ main.rb
@@ -1,16 +1,16 @@
-def draw_dark_mode_button(label_text, button_location, foreground_color)
-    paint(label_text, button_location.x, button_location.y, foreground_color - 10, '#111111')
-end
-
-def draw_normal_button(label_text, button_location, foreground_color)
-    paint(label_text, button_location.x, button_location.y, foreground_color + 10, '#E0E0E0')
-end
-
 class Button
     def initialize(label_text, button_location, foreground_color)
         @label_text = label_text
         @button_location = button_location
         @foreground_color = foreground_color
+    end
+
+    def draw_dark_mode_button(label_text, button_location, foreground_color)
+        paint(label_text, button_location.x, button_location.y, foreground_color - 10, '#111111')
+    end
+    
+    def draw_normal_button(label_text, button_location, foreground_color)
+        paint(label_text, button_location.x, button_location.y, foreground_color + 10, '#E0E0E0')
     end
 end
 
