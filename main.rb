07c84bfdd83d93a2dde7024ff6fb53a7890777dd
Micah Stalberg <stalberm@oregonstate.edu>
2022-05-08 18:15:34 -0700

    Create new button class

--- main.rb
+++ main.rb
@@ -6,6 +6,10 @@ def draw_normal_button(label_text, button_location, foreground_color)
     paint(label_text, button_location.x, button_location.y, foreground_color + 10, '#E0E0E0')
 end
 
+class Button
+
+end
+
 class ButtonLocation
     attr_reader :x, :y
     def initialize(x,y)
