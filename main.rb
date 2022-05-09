cf7dc8b252d6435752fb8f1f6871d64bc0de8584
Micah Stalberg <stalberm@oregonstate.edu>
2022-05-08 18:10:11 -0700

    Remove old draw_button function

--- main.rb
+++ main.rb
@@ -1,13 +1,3 @@
-def draw_button(label_text, button_location, foreground_color, is_dark_mode)
-    if is_dark_mode
-      # darken foreground color for dark mode
-      paint(label_text, button_location.x, button_location.y, foreground_color - 10, '#111111')
-    else
-      # lighten foreground color for non-dark mode
-      paint(label_text, button_location.x, button_location.y, foreground_color + 10, '#E0E0E0')
-    end
-end
-
 def draw_dark_mode_button(label_text, button_location, foreground_color)
     paint(label_text, button_location.x, button_location.y, foreground_color - 10, '#111111')
 end
